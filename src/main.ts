8eeb8374db38715f938b4069f9c9ff576946ce51
ElectroCERP <carloscer27r@gmail.com>
2025-09-17 16:45:23 -0300

    Update API bootstrap method

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import { setupApp, setupSwagger } from './config/app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.enableCors(configService.get('frontend.url'));
+
+  setupApp(app);
+  setupSwagger(app);
+
+  const port = configService.get('server.port');
+  await app.listen(port);
+
+  console.log(`Server is running on port ${port}`);
 }
 bootstrap();
