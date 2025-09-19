33b4bd82b35aa07e6bccce4c69ce6fbf9e99f018
ElectroCERP <carloscer27r@gmail.com>
2025-09-19 11:05:46 -0300

    Init prisma orm

---
+++ prisma/migrations/20250919132825_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "back_template"."User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "back_template"."User"("email");
